f8a181b6d425dbafcff4b331e7eeeb76467406e0
CP363524 <misbacheulkar@capitecbank.co.za>
2024-03-19 14:24:41 +0200

    Added ChatMessageRepository

---
+++ src/main/java/com/chatServer/ChatMessageRepository.java
@@ -0,0 +1,13 @@
+
+package com.chatServer;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+import java.util.List;
+
+@Repository
+public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
+    List<ChatMessage> findByUsername(String username);
+}
